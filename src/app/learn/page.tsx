e8c610f9aba80edcb96b39a9493119ff58374e41
Gary Xia (myopicOracle) <184602237+myopicOracle@users.noreply.github.com>
2026-05-03 22:07:46 +0800

    Refactor /learn to use Search Params, collapse project page logic into dashboard

--- src/app/learn/page.tsx
+++ src/app/learn/page.tsx
@@ -1,24 +1,61 @@
 'use client'
 
 import { useState } from 'react'
-import { sora } from '@/app/ui/fonts'
+import { useSearchParams, useRouter } from 'next/navigation'
+import { sora, ibmPlexMono, manrope } from '@/app/ui/fonts'
 import ThemeSelect from '@/components/ThemeSelect'
 import ModuleCard from '@/components/ModuleCard'
-import { projectData } from '@/data/modules'
+import { projectData, skillsData } from '@/data/modules'
 
 export default function Page() {
     const [theme, setTheme] = useState('raisin')
+    const searchParams = useSearchParams()
+    const router = useRouter()
+    const moduleID = searchParams.get('module')
+    const project = moduleID
+        ? projectData.find((project) => project.id === moduleID)
+        : null
 
-    return (
+    const header = (
+        <div className={`flex justify-between bg-${theme}-page p-2`}>
+            <h1 className={`${sora.className} text-2xl`}>Raisin.IDE</h1>
+            <ThemeSelect theme={theme} setTheme={setTheme} />
+        </div>
+    )
+
+    return project ? (
+        <main
+            className={`
+                flex flex-col w-full h-screen
+                bg-${theme}-gap text-${theme}-font-primary 
+                overflow-hidden
+                `}>
+            {header}
+            <div className="flex-none p-2">
+                <h2
+                    className={`${ibmPlexMono.className} text-xl font-bold mb-4`}>
+                    {project.name}
+                </h2>
+            </div>
+            <div className="flex-1 flex flex-col gap-12 p-3">
+                {project.skills.map((skill, index) => (
+                    <div
+                        key={index}
+                        className="flex flex-col text-lg font-semibold gap-2">
+                        <h1
+                            className={`${manrope.className} text-${theme}-font-tertiary`}>
+                            {skill}
+                        </h1>
+                        <div className="text-sm font-normal">{`<Content/>`}</div>
+                    </div>
+                ))}
+            </div>
+            <button>Start Project</button>
+        </main>
+    ) : (
         <main
             className={`flex flex-col w-full h-screen bg-${theme}-gap overflow-hidden`}>
-            <div className={`flex justify-between bg-${theme}-page p-2`}>
-                <h1
-                    className={`${sora.className} text-${theme}-font-primary text-2xl`}>
-                    Raisin.IDE
-                </h1>
-                <ThemeSelect theme={theme} setTheme={setTheme} />
-            </div>
+            {header}
             <div className="flex-none p-6 overflow-auto">
                 <h2
                     className={`text-${theme}-font-tertiary text-4xl font-semibold mb-4`}>
