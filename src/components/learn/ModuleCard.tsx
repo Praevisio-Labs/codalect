791fef901c0b4274d42c496ad382d83c02975768
Gary Xia (myopicOracle) <184602237+myopicOracle@users.noreply.github.com>
2026-05-16 10:18:28 +0800

    Refactor basic ModuleCard layout and remove teachers logic entirely

--- src/components/learn/ModuleCard.tsx
+++ src/components/learn/ModuleCard.tsx
@@ -1,11 +1,12 @@
 import { ModuleProps } from '@/types/components'
-import { teachers } from '@/data/learn/teachers'
-import { CalendarDaysIcon, ClockIcon } from '@heroicons/react/24/outline'
+import {
+    RocketLaunchIcon,
+    CalendarDaysIcon,
+    ClockIcon,
+} from '@heroicons/react/24/outline'
 
 export default function ModuleCard({ project, onClick }: ModuleProps) {
-    const teacherId = project.teachers[0]
-    const teacher = teachers.find((person) => person.id === teacherId)
-    const isComingSoon = project.comingSoon === true
+    const isComingSoon = project.isReleased === false
     const cardStateClasses = isComingSoon
         ? 'opacity-70 cursor-default'
         : 'hover:opacity-60 cursor-pointer'
@@ -21,58 +22,57 @@ export default function ModuleCard({ project, onClick }: ModuleProps) {
                     bg-input
                     ${cardStateClasses}
                     `}>
-            {/* Original boilerplate from HyperUI - https://hyperui.dev/components/marketing/cards/*/}
-            <div className="w-full sm:flex sm:items-start sm:justify-between sm:gap-4 lg:gap-6">
-                <div className="sm:order-last sm:shrink-0">
-                    <img
-                        alt={`Image of ${teacher?.name ?? 'Teacher'}`}
-                        src={teacher?.avatar}
-                        className="size-20 rounded-full object-cover"
-                    />
-                </div>
+            <div className="flex items-start sm:gap-8">
+                <div className="hidden sm:block sm:size-20 sm:shrink-0 sm:rounded-full sm:overflow-hidden"></div>
 
-                <div className="mt-4 sm:mt-0 sm:flex-1">
-                    <h3 className="text-lg font-medium text-pretty text-font-apex">
+                <div>
+                    <strong className="rounded-sm border border-accent-muted px-3 py-1.5 text-[10px] font-medium text-font-secondary">
+                        {project.level}
+                    </strong>
+
+                    <h3 className="mt-4 text-lg font-medium text-font-apex sm:text-xl">
                         {project.name}
                     </h3>
 
-                    <p className="mt-1 text-sm text-font-paragraph">
-                        By {teacher?.name ?? 'Unknown Teacher'}
-                    </p>
-
-                    <p className="mt-4 line-clamp-2 text-sm text-pretty text-font-paragraph">
+                    <p className="mt-1 line-clamp-3 text-sm text-pretty text-font-paragraph">
                         {project.description}
                     </p>
+
+                    <div className="mt-4 sm:flex sm:items-center sm:gap-2">
+                        {isComingSoon ? (
+                            <div className="flex items-center gap-1 text-font-secondary">
+                                <ClockIcon
+                                    className="size-4"
+                                    aria-hidden="true"
+                                />
+                                <p className="text-xs font-medium">
+                                    Coming soon
+                                </p>
+                            </div>
+                        ) : (
+                            <>
+                                <div className="flex items-center gap-1 text-font-secondary">
+                                    <ClockIcon
+                                        className="size-4"
+                                        aria-hidden="true"
+                                    />
+                                    <p className="text-xs font-medium">
+                                        {project.duration}
+                                    </p>
+                                </div>
+
+                                <span
+                                    className="hidden sm:block text-font-secondary"
+                                    aria-hidden="true">
+                                    ·
+                                </span>
+
+                                <p className="mt-2 text-xs font-medium text-font-secondary sm:mt-0"></p>
+                            </>
+                        )}
+                    </div>
                 </div>
             </div>
-
-            {isComingSoon ? (
-                <div className="mt-auto flex items-center gap-2 text-xs text-font-secondary">
-                    <ClockIcon
-                        className="size-5 text-font-secondary"
-                        aria-hidden="true"
-                    />
-                    <span>Coming soon...</span>
-                </div>
-            ) : (
-                <div className="mt-auto flex items-center gap-4 text-xs text-font-secondary">
-                    <div className="flex items-center gap-2">
-                        <CalendarDaysIcon
-                            className="size-5 text-font-secondary"
-                            aria-hidden="true"
-                        />
-                        <span>April 21, 2026</span>
-                    </div>
-
-                    <div className="flex items-center gap-2">
-                        <ClockIcon
-                            className="size-5 text-font-secondary"
-                            aria-hidden="true"
-                        />
-                        <span>92 minutes</span>
-                    </div>
-                </div>
-            )}
         </div>
     )
 }
